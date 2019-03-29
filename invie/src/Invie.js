7708f0b98d8dc9b3b32be8098748dc25a676af40
lmayai <leonardomayairegui@gmail.com>
2019-03-29 16:50:16 -0500

    Inicio de componente de portada

--- invie/src/Invie.js
+++ invie/src/Invie.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './Invie.css';
+import Portada from './components/Portada.jsx'
 
 class Invie extends Component {
   render() {
     return (
       <section className="Invie">
+        <Portada />
         {/*PORTADA*/}
         {/*GUITARRAS*/}
         {/*FOOTER*/}
---
+++ invie/src/components/Portada.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Portada extends Component {
+  render() {
+    return (
+      <div>Hola portada</div>
+    )
+  }
+}
+
+export default Portada

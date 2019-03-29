9178e9707e897f4d000023719711902f9230bfed
lmayai <leonardomayairegui@gmail.com>
2019-03-29 12:26:30 -0500

    Uso de animacinones usando transitions groups

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'; // ES6
+import logoPlatzi from './logo-platzi.png'
 
 class App extends Component {
+  // El estado de las variables que quiero cambiar
+  state = {
+    logo:logo,
+  }
+  onClick(){
+    this.setState({
+      logo:logoPlatzi,
+    })
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <ReactCSSTransitionGroup
+            transitionName="fade" 
+            transitionEnterTimeout={1000}  // Duracion en ms de las ani
+            transitionLeaveTimeout={1000}
+          >
+            <img 
+              key={this.state.logo} //Cada que haya un cambio hace leave
+              src={this.state.logo} 
+              className="App-logo" 
+              alt="logo" />
+          </ReactCSSTransitionGroup>
+          <button onClick={this.onClick.bind(this)} >CLick me</button>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>

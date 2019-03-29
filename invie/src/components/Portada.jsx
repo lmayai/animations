dde77bd58a2212d1a5031d65c207d4df9762fe0a
lmayai <leonardomayairegui@gmail.com>
2019-03-29 17:00:05 -0500

    Iamgenes y portada

--- invie/src/components/Portada.jsx
+++ invie/src/components/Portada.jsx
@@ -3,7 +3,32 @@ import React, { Component } from 'react';
 class Portada extends Component {
   render() {
     return (
-      <div>Hola portada</div>
+      <section id="portada" className="portada background">
+      <header id="header" className="header contenedor"> 
+        <figure className="logotipo"> 
+          <img src="images/invie.png" srcset="images/invie.png 1x, images/invie2x.png 2x" width="186" height="60" alt="Invie logotipo"/>
+        </figure>
+        <span className="burguer-button icon-menu" id="burguer-button"></span>
+        <nav className="menu" id="menu"> 
+          <ul>
+            <li>
+              <a href="index.html">Home</a>
+            </li>
+            <li>
+              <a href="#guitarras">Guitarras</a>
+            </li>
+            <li>
+              <a href="precios.html">Precios</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <div className="contenedor">
+        <h1 className="titulo">Guitarras <span>invie</span>sibles</h1>
+        <h3 className="title-a">Sé la estrella de rock que siempre quisiste ser</h3>
+        <a className="button" href="#guitarras">Conoce mas</a>
+      </div>
+    </section>
     )
   }
 }
